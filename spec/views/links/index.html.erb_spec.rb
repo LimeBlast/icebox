767303e043ea1d3b928893262fe79ab4c11f8c0c
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-02-10 16:36:03 +0000

    added link/index spec and view

--- spec/views/links/index.html.erb_spec.rb
+++ spec/views/links/index.html.erb_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'links/index.html.erb', :type => :view do
+  let(:links) { build_list(:link, 3, id: 1) }
+
+  before :each do
+    assign(:links, links)
+    render
+  end
+
+  it 'shows the page title' do
+    expect(rendered).to have_css('h1', 'Links')
+  end
+
+  it 'displays a list of links' do
+    expect(rendered).to have_link(links[0].title, :href => link_path(links[0]))
+    expect(rendered).to have_link(links[1].title, :href => link_path(links[1]))
+    expect(rendered).to have_link(links[2].title, :href => link_path(links[2]))
+  end
 end
