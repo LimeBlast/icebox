9e574dfd2d750aae5807e9811bf15fc919cd003e
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-02-10 16:36:41 +0000

    added show route to links resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :links, only: [:index, :new, :create]
+  resources :links, only: [:index, :show, :new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
