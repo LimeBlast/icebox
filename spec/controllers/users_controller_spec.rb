0594c58d4586f9b7794c7367ec7da526b4a55551
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-02-19 15:37:40 +0000

    refactored the user_controller_spec to be a little less messy

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -25,34 +25,39 @@ RSpec.describe UsersController, :type => :controller do
 
     let(:register_user_params) { { email: email, password: password, password_confirmation: password } }
     let(:register_user_form_class) { class_double('RegisterUserForm').as_stubbed_const }
-    let(:register_user_form_object) { double('RegisterUserForm') }
+    let(:register_user_form_object) { double('RegisterUserForm', valid?: valid?, result: user_object) }
     let(:user_object) { build(:user) }
 
     before :each do
-      allow(register_user_form_object).to receive(:valid?).and_return(true)
       allow(register_user_form_class).to receive(:run).and_return(register_user_form_object)
-      allow(register_user_form_object).to receive(:result).and_return(user_object)
     end
 
-    it 'attempts to creates a new user' do
-      post :create, register_user: register_user_params
-      expect(assigns(:register_user)).to eq(register_user_form_object)
+    # it 'attempts to creates a new user' do
+    #   post :create, register_user: register_user_params
+    #   expect(assigns(:register_user)).to eq(register_user_form_object)
+    # end
+
+    context 'when register user is successful' do
+      let(:valid?) { true }
+
+      it 'attempts to log the user in' do
+        expect(controller).to receive(:auto_login).with(user_object)
+        post :create, register_user: register_user_params
+      end
+
+      it 'redirects to the dashboard path' do
+        post :create, register_user: register_user_params
+        expect(response).to redirect_to(dashboard_path)
+      end
     end
 
-    it 'attempts to log the user in' do
-      expect(controller).to receive(:auto_login).with(user_object)
-      post :create, register_user: register_user_params
-    end
+    context 'when register user is unsuccessful' do
+      let(:valid?) { false }
 
-    it 'redirects to the dashboard path if successful' do
-      post :create, register_user: register_user_params
-      expect(response).to redirect_to(dashboard_path)
-    end
-
-    it 'renders the new template if unsuccessful' do
-      allow(register_user_form_object).to receive(:valid?).and_return(false)
-      post :create, register_user: register_user_params
-      expect(response).to render_template(:new)
+      it 'renders the new template' do
+        post :create, register_user: register_user_params
+        expect(response).to render_template(:new)
+      end
     end
   end
 end
