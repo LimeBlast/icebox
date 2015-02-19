89ba9625c3a26bccd31a966b08cd499c681e6746
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-02-19 12:03:17 +0000

    Refactored the sign up form to use ActiveInteraction, but the AI class itself is not unit tested - that's next, after I've installed simple form

--- app/forms/register_user.rb
+++ app/forms/register_user.rb
@@ -1,5 +1,23 @@
 class RegisterUser < ActiveInteraction::Base
   string :email, :password, :password_confirmation
 
-  validates :url, :title, :body, presence: true
+  validates :password, length: { minimum: 3 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+
+  validate :email_uniqueness
+
+  def execute
+    User.create!(inputs)
+  end
+
+  private
+
+    def email_uniqueness
+      errors.add_sym(:email, :not_unique) unless email_unique?
+    end
+
+    def email_unique?
+      !User.exists?(email: email)
+    end
 end
