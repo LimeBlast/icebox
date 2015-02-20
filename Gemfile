92ac282a87628dfc00e4f3f18dcfa50ada5e6e16
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-02-20 12:14:35 +0000

    moved sqlite into the dev and test group (to stop heroku from moaning about it), run a bundle update, and added the new set-up required for rspec-html-matchers gem

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 ruby '2.2.0'
 
 gem 'rails', '4.2.0'
-gem 'sqlite3'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
@@ -16,6 +15,7 @@ gem 'active_interaction', '~> 1.5'
 gem 'formtastic'
 
 group :development, :test do
+  gem 'sqlite3'
   gem 'byebug'
   gem 'web-console', '~> 2.0'
   gem 'spring'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   it { should validate_presence_of(:email) }
-  it { should ensure_length_of(:password).is_at_least(3) }
+  it { should validate_length_of(:password).is_at_least(3) }
   it { should validate_confirmation_of(:password) }
   it { should validate_presence_of(:password_confirmation) }
   it { should validate_uniqueness_of(:email) }
